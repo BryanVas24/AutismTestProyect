027d555d95454cf6ab4af307b11f85c1e9ddf41a
xaldakr <xavieravilaposada@gmail.com>
2025-06-06 12:21:17 -0600

    Test my tseT

--- src/types/general.ts
+++ src/types/general.ts
@@ -8,3 +8,31 @@ export type User = {
   rol: number;
   telefono: string;
 };
+
+export type Test = {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  sistema_puntaje: string;
+  edad_min: number;
+  edad_max: number;
+  preguntas: Pregunta[];
+  requesterId: null | number;
+}
+
+export type Pregunta = {
+  id: number;
+  pregunta: string;
+  num_pregunta: string;
+  testId: number;
+  puntajepregs: PuntajePreg[];
+  requesterId: null | number;
+}
+
+export type PuntajePreg = {
+  id: number;
+  nombre: string;
+  valor: number;
+  preguntaId: number;
+  requesterId: null | number;
+}
